ee8b9859002f5c129a23234f7d6cdcedf6a4844a
K.Sriyesh <sriyesh1999@users.noreply.github.com>
2020-11-07 12:18:29 +0530

    deploy

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { connect } = require('./routes/auth');
-
+const path =require('path');
 const connectDB=require('./config/db')
 const app = express();
 connectDB();
@@ -9,10 +9,15 @@ connectDB();
 app.use(express.json({extended: false}))
 
 
-const PORT=process.env.PORT ||1805;
+
 //define Routes
 app.use('/api/users',require('./routes/users'))
 app.use('/api/contact',require('./routes/contact'))
 app.use('/api/auth',require('./routes/auth'))
-app.get('/',(req,res) =>res.json({msg:"welcome"}))
+const PORT=process.env.PORT ||1805;
+if(process.env.NODE_ENV==='production'){
+    app.use(express.static('client/build'))
+    app.get('*',(req,res)=>res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    )
+}
 app.listen(PORT,()=>console.log(PORT))
